ff7ab8eca3b0cbefef1622d0a5da4a6f888c4481
Rohit Kumar <rohitkk074@gmail.com>
2021-01-07 10:18:22 +0530

    Added Error Handling

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const globalErrorHandler = require('./controller/errorController');
 app = express();
 //MIDDLEWARES.
 if(process.env.NODE_ENV === 'development') {
@@ -15,5 +16,15 @@ app.use(express.static((`${__dirname}/public`)))
 // ROUTE HANDLERS FOR USER.
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*',(req,res,next) => {
+    // const err = new Error(`Can't find the ${req.originalUrl} on this server!`);
+    // err.status = 'fail';
+    // err.statusCode = 404;
+
+    next(new AppError(`Can't find the ${req.originalUrl} on this server!`,404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app
