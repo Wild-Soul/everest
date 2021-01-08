04f7311ec46062674d2282da3f7534457019c204
Rohit Kumar <rohitkk074@gmail.com>
2021-01-08 22:24:26 +0530

    Uncaught exception and rejection handling

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception! => Shutting down');
+  console.log(err.name, err.message);
+  process.exit(1);
+})
+
 dotenv.config( {path: './config.env'} );
 const app = require('./app');
-const mongoose = require('mongoose');
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DB_PASSWD);
 // console.log(process.env);
@@ -21,6 +28,14 @@ const port = process.env.PORT || 3001;
 // console.log(app);
 
 // console.log(app)
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
+
+process.on('unhadledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandeled Rejection => Shutting down');
+  server.close(() =>{
+    process.exit(1);
+  })
+})
