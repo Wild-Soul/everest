aa9fee74f9156eb7e467bc3ec9f99386640cec27
Rohit Kumar <rohitkk074@gmail.com>
2021-02-01 23:31:44 +0530

    added input sanitization and parameter pollution

--- app.js
+++ app.js
@@ -3,14 +3,42 @@ const morgan = require("morgan");
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./controller/errorController');
+const { mongo } = require("mongoose");
+
 app = express();
 //MIDDLEWARES.
+app.use(helmet());
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.json());
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60*60*1000,
+    message: 'Too many request fro this IP, please try again in an hour'
+});
+
+app.use('/api', limiter);
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp({
+    whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}));
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static((`${__dirname}/public`)))
 
 // ROUTE HANDLERS FOR USER.
