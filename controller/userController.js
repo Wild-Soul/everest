504e631ba8d0cd5f971cb41cc6e06662076b5ebe
Rohit Kumar <rohitkk074@gmail.com>
2021-02-23 23:37:13 +0530

    Factory function for create and update

--- controller/userController.js
+++ controller/userController.js
@@ -33,13 +33,6 @@ exports.createUser = (req,res) => {
     });
 }
 
-exports.updateUser = (req,res) => {
-    res.status(500).json({
-        status: "error",
-        message: "This route is not yet defined"
-    });
-}
-
 exports.getUser = (req,res) => {
     res.status(500).json({
         status: "error",
@@ -83,4 +76,5 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     })
 });
 
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
