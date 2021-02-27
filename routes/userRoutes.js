0e00083b57ed9411ecd636e376a41603ed969a15
Rohit Kumar <rohitkk074@gmail.com>
2021-02-28 00:35:15 +0530

    modified authorization and authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,18 +6,26 @@ const reviewController = require('../controller/reviewController');
 
 
 //ROUTES FOR USERS.
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Protect all the routes below.
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+router.get('/me', userController.getMe, userController.getUser);
+
+router.use(authController.restrictTo('admin'));
+
 router
     .route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser);
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
 router
     .route('/:id')
     .get(userController.getUser)
